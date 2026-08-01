078ee646f04fdda762b9226b693fe44b8c302566
adef7626 <adef7626@gmail.com>
2026-08-01 14:05:31 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * ==========================================================================
  */
 
-const CACHE_NAME = "adrenaline-cache-v62";
+const CACHE_NAME = "adrenaline-cache-v63";
 
 const ASSETS_TO_CACHE = [
   "./",
